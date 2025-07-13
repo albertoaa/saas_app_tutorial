a897ffcd9a4f9dd5c7f8616e217dea614ff80831
Alberto Aragón Alvarez <ronin8503@gmail.com>
2025-07-13 23:53:43 +0100

    feat: add shadcn button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+  return <div className='text-2xl underline'>Welcome to my SaaS App</div>;
+};
 
-export default Page
+export default Page;
