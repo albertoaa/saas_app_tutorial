6a3574e3bfef48fbc3fa57e282c5b0989ab80346
Alberto Aragón Alvarez <ronin8503@gmail.com>
2025-07-13 23:56:24 +0100

    feat: initial page commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Page = () => {
-  return <div className='text-2xl underline'>Welcome to my SaaS App</div>;
+  return (
+    <>
+      <div className='text-2xl underline'>Welcome to my SaaS App </div>
+      <Button>{`Let's get started`}</Button>
+    </>
+  );
 };
 
 export default Page;
